da28902a523ecefa4518690d4a469034c3248a05
hanbyul_kim <rlagksquf30@gmail.com>
2017-01-30 10:05:04 +0000

    sms service

---
+++ app/controllers/tm_controller.rb
@@ -0,0 +1,29 @@
+class TmController < ApplicationController
+  
+    def index
+    end
+  def sendtest
+    accountSID = ENV['TWILIO_SID']
+    authToken = ENV['TWILIO_AUTH']
+    @client = Twilio::REST::Client.new accountSID, authToken #Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token  
+    from = ENV['BASE_NUM']
+    friends = {'+821065101264' => "HBK"}
+    
+    #frineds.each do |key, value|
+      message = @client.account.messages.create(
+        :from=> from,
+        :to=> '+821065101264',
+        :body=> "Key gogo, textmessage"
+      )
+      puts "Sent message"
+    #end
+    
+    redirect_to '/'
+  end
+#    skip_before_action :verify_authenticity_token
+ # rescue_from StandardError do |exception|
+  #  trigger_sms_alerts(exception)
+  #end
+
+  
+end
